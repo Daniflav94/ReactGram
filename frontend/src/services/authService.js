fec5eddce5c3f87662042b26a19fb1c30caaf8e6
Daniflav94 <danyelly_mell@hotmail.com>
2023-05-14 21:12:26 -0300

    Método de login implementado em authservice

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -6,8 +6,8 @@ const register = async (data) => {
 
   try {
     const res = await fetch(api + "/users/register", config)
-    .then((res) => res.json())
-    .catch((err) => err);
+    .then((res) => res.json()) //transforma a resposta em json
+    .catch((err) => err); //pega possíveis erros da requisição e nos retorna
 
     if(res){ //backend retorna id e token do usuário
         localStorage.setItem("user", JSON.stringify(res))
@@ -25,9 +25,30 @@ const logout = () => {
   localStorage.removeItem("user")
 }
 
+//Sign in an user
+const login = async(user) => {
+
+  const config = requestConfig("POST", user)
+
+  try {
+    
+    const res = await fetch(api + "/users/login", config).then((res) => res.json()).catch((err) => err)
+
+    if(res){
+      localStorage.setItem("user", JSON.stringify(res))
+    }
+
+    return res
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const authService = {
     register,
     logout,
+    login
 }
 
 export default authService
