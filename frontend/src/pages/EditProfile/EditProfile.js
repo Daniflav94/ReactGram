36d0591e99e4aa67147daf1d2c560a73876f4817
Daniflav94 <danyelly_mell@hotmail.com>
2023-05-18 11:29:25 -0300

    Update user

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -1,33 +1,98 @@
-import './EditProfile.css'
+import "./EditProfile.css";
+import { uploads } from "../../utils/config";
+
+import { profile, resetMessage } from "../../slices/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import Message from "../../components/Message/Message";
 
 const EditProfile = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault()
+  const dispatch = useDispatch();
+
+  const { user, message, error, loading } = useSelector((state) => state.user);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [bio, setBio] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  //Load user data
+  useEffect(() => {
+    dispatch(profile());
+    console.log(user)
+  }, [dispatch]);
+
+  //Fill form with user data
+  useEffect(() => {
+
+    if(user){ //pode vir vazio inicialmente, para garantir fazer o if(user)
+        setName(user.name)
+        setEmail(user.email)
+        setBio(user.bio)
     }
+
+  }, [user])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleFile = (e) => {
+    //image preview
+    const image = e.target.files[0]
+
+    setPreviewImage(image)
+    setProfileImage(image)
+  }
 
   return (
     <div id="edit-profile">
-        <h2>Edite seus dados</h2>
-        <p className="subtitle">Adicione uma imagem de perfil e conte mais sobre você...</p>
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder='Nome' />
-            <input type="email" placeholder='E-mail' disabled/>
-            <label>
-                <span>Imagem do Perfil:</span>
-                <input type="file" />
-            </label>
-            <label>
-                <span>Bio:</span>
-                <input type="text" placeholder="Descrição do perfil" />
-            </label>
-            <label>
-                <span>Quer alterar sua senha?</span>
-                <input type="password" placeholder="Digite sua nova senha"/>
-            </label>
-            <input type="submit" value="Atualizar" />
-        </form>
+      <h2>Edite seus dados</h2>
+      <p className="subtitle">
+        Adicione uma imagem de perfil e conte mais sobre você...
+      </p>
+      {(user.profileImage || previewImage) && (
+        <img className="profile-image" src={previewImage ? URL.createObjectURL(previewImage) : `${uploads}/users/${user.profileImage}`} alt={user.name} />
+      )}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ""}
+        />
+        <input type="email" placeholder="E-mail" disabled value={email || ""} />
+        <label>
+          <span>Imagem do Perfil:</span>
+          <input type="file" onChange={handleFile}/>
+        </label>
+        <label>
+          <span>Bio:</span>
+          <input
+            type="text"
+            placeholder="Descrição do perfil"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio || ""}
+          />
+        </label>
+        <label>
+          <span>Quer alterar sua senha?</span>
+          <input
+            type="password"
+            placeholder="Digite sua nova senha"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password || ""}
+          />
+        </label>
+        {!loading && <input type="submit" value="Atualizar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
+        {message && <Message msg={message} type="success" />}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default EditProfile
+export default EditProfile;
